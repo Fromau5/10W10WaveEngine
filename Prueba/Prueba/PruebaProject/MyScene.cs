5ba695fc32eab910e6bc335cda57929fac43ed41
Jesús <Fromau5@users.noreply.github.com>
2015-07-05 04:43:46 +0200

    Subiendo un proyecto de prueba. Se han creado un par de entidades que se muestran rotando por pantalla.

---
+++ Prueba/Prueba/PruebaProject/MyScene.cs
@@ -0,0 +1,90 @@
+#region Using Statements
+using System;
+using WaveEngine.Common;
+using WaveEngine.Common.Graphics;
+using WaveEngine.Common.Math;
+using WaveEngine.Components.Cameras;
+using WaveEngine.Components.Graphics2D;
+using WaveEngine.Components.Graphics3D;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Resources;
+using WaveEngine.Framework.Services;
+using WaveEngine.Materials;
+#endregion
+
+namespace PruebaProject
+{
+    public class MyScene : Scene
+    {
+        protected override void CreateScene()
+        {
+            //Insert your scene definition here.
+
+            var camera = new FreeCamera("Main", new Vector3(0, 2, 4), Vector3.Zero)
+            {
+                BackgroundColor = Color.White
+            };
+            EntityManager.Add(camera);
+
+            /*
+            Entity cube = new Entity()
+                .AddComponent(Model.CreateCube())
+                .AddComponent(new MaterialsMap(new BasicMaterial("Content/crate.wpk")))
+                .AddComponent(new Transform3D())
+                .AddComponent(new ModelRenderer())
+                .AddComponent(new Spinner() { AxisTotalIncreases = Vector3.One / 2 });
+
+            EntityManager.Add(cube);
+            */
+            
+            Entity cone = new Entity()
+                .AddComponent(Model.CreateCone())
+                .AddComponent(new MaterialsMap(new BasicMaterial("Content/crate.wpk")))
+                .AddComponent(new Transform3D())
+                .AddComponent(new ModelRenderer())
+                .AddComponent(new Spinner() { AxisTotalIncreases = Vector3.One / 2 });
+
+            EntityManager.Add(cone);
+
+            /*
+            #region Simple test
+            //Create a 3D camera
+            var camera3D = new FreeCamera("Camera3D", new Vector3(0, 2, 4), Vector3.Zero) { BackgroundColor = Color.CornflowerBlue };
+            EntityManager.Add(camera3D);
+
+            // Draw a cube
+            Entity cube = new Entity()
+                .AddComponent(new Transform3D())
+                .AddComponent(Model.CreateCube())
+                .AddComponent(new Spinner() { AxisTotalIncreases = new Vector3(1, 2, 3) })
+                .AddComponent(new MaterialsMap())
+                .AddComponent(new ModelRenderer());
+
+            EntityManager.Add(cube);
+
+            // Create a 2D camera
+            var camera2D = new FixedCamera2D("Camera2D") { ClearFlags = ClearFlags.DepthAndStencil }; // Transparent background need this clearFlags.
+            EntityManager.Add(camera2D);
+
+            // Draw a simple sprite
+            Entity sprite = new Entity()
+                .AddComponent(new Transform2D())
+                // Change this line for a custom assets "new Sprite("Content/MyTexture"))"
+                // Manage assets using the Resources.weproj link to open the Assets Exporter tool.
+                .AddComponent(new Sprite(StaticResources.DefaultTexture))
+                .AddComponent(new SpriteRenderer(DefaultLayers.Opaque));
+
+            EntityManager.Add(sprite);
+            #endregion
+            */
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            // This method is called after the CreateScene and Initialize methods and before the first Update.
+        }
+    }
+}
